50fd73d9b41ef07344bef73cacc431a67ff1c9bc
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-12-14 15:46:41 -0800

    added BrowseRouter/Navlink code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <NavLink to="/films" data-testid="film-link">
+            Films
+          </NavLink>
+          <NavLink to="/characters" data-testid="char-link">
+            Characters
+          </NavLink>
+          <NavLink to="/books" data-testid="book-link">
+            Books
+          </NavLink>
+          <NavLink to="/home" data-testid="home-link">
+            Home
+          </NavLink>
+        </header>
+        <Switch>
+          <Route path="/films">
+            <FilmList />
+          </Route>
+          <Route path="/characters">
+            <CharacterList />
+          </Route>
+          <Route path="/books">
+            <BooksList />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
